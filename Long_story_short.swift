e8b823735d6c687a7b60a92ba75940e6350a71a8
L <60981745+pankourgiou@users.noreply.github.com>
2024-11-11 09:29:30 +0200

    Create Long_story_short.swift

---
+++ Long_story_short.swift
@@ -0,0 +1,6 @@
+//four relatives talk...and disconnected album or guns n' roses album haunts them...
+print("Can you give me some wa-wa-water?")//one says
+print("o-o-ok")//brother says
+print("k")//sister says
+print("is this ma-ma-math?")//I say
+//What we've got here is...failure to communicate...that's truth!this is a mediocre familly story
